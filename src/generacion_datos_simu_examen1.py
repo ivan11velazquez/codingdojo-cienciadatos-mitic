a323fe73b3d11608c58e51280416b6f0c5ce13e5
ivelazquez <ivelazquez@isaportal.com.py>
2024-10-24 01:59:30 -0300

    * Subida la tarea core analisis_panda_kaggle * Subida la tarea simu_examen1 * Se movieron las tareas core a su correspondiente carpeta

---
+++ src/generacion_datos_simu_examen1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import random
+
+# Configuramos la semilla para reproducibilidad
+np.random.seed(42)
+
+# Generamos los datos sintéticos
+#user_id = range(1, 1000)
+user_id = np.random.randint(1,1000)
+app_version = np.random.choice(['1.0', '1.1', '1.2', '1.3'], 300)
+platform = np.random.choice(['Android', 'iOS', 'anDROId', 'ios', 'Ps5', 'XboX'], 300)
+session_duration = np.random.randint(1, 1440, 300)
+number_of_sessions = np.random.randint(1, 1440, 300)
+country = np.random.choice(['USA', 'Canada', 'Mexico', 'UK', 'Germany', 'France', 'Spain', 'Italy'], 300)
+user_feedback = np.random.randint(0, 10, 300)
+
+# Creamos el DataFrame
+data = pd.DataFrame({
+    'user_id': user_id,
+    'app_version': app_version,
+    'platform': platform,
+    'session_duration': session_duration,
+    'number_of_sessions': number_of_sessions,
+    'country': country,
+    'user_feedback': user_feedback
+})
+
+# Guardamos los datos en un archivo CSV
+data.to_csv('user_app_data.csv', index=False)
+print("Datos generados y guardados en 'user_app_data.csv'")
---
+++ src/metodos_auxiliares_simu_examen1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+df = pd.read_csv('../data/user_app_data.csv')
+df.head(10)
